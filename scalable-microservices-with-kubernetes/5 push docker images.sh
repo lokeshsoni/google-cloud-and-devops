3ea4f6c825fc84cbd56dfba0d7e5d560dbcd5074
Lokesh Soni <ynkshatriya@gmail.com>
2018-11-26 19:41:51 +0530

    Mon Nov 26 18:38:32 IST 2018

--- scalable-microservices-with-kubernetes/5 push docker images.sh
+++ scalable-microservices-with-kubernetes/5 push docker images.sh
@@ -1,11 +1,6 @@
-# Add your own tag
-sudo docker tag monolith:1.0.0 <your username>/monolith:1.0.0
-For example (you can rename too!)
+# Add tag to image
+docker tag monolith:1.0.0 lokeshsonii/monolith:1.0.0
 
-sudo docker tag monolith:1.0.0 udacity/example-monolith:1.0.0
-Create account on Dockerhub
-To be able to push images to Dockerhub you need to create an account there - https://hub.docker.com/register/
-
-Login and use the docker push command
-sudo docker login
-sudo docker push udacity/example-monolith:1.0.0
+# Login and push the docker image
+docker login
+docker push lokeshsonii/monolith:1.0.0
