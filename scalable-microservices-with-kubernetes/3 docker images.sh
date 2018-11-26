8b8c558735c2d85e4ee0868730335f7d963cd37b
Lokesh Soni <ynkshatriya@gmail.com>
2018-11-26 18:37:39 +0530

    Mon Nov 26 18:37:39 IST 2018

--- scalable-microservices-with-kubernetes/3 docker images.sh
+++ scalable-microservices-with-kubernetes/3 docker images.sh
@@ -10,3 +10,16 @@ sudo docker images
 
 # Run the first instance
 sudo docker run -d nginx:1.10.0
+
+# Check if it's up
+sudo docker ps
+
+# Run a different version of nginx
+sudo docker run -d nginx:1.9.3
+
+# Run another version of nginx
+sudo docker run -d nginx:1.10.0
+
+# Check how many instances are running
+sudo docker ps
+sudo ps aux | grep nginx
